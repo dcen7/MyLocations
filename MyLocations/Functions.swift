862c5c8328bf55c22f434834291b803ce04590b4
dcen7 <mdcengiz@gmail.com>
2022-11-16 14:44:12 -0600

    Handle Core Data errors

--- MyLocations/Functions.swift
+++ MyLocations/Functions.swift
@@ -15,3 +15,10 @@ let applicationDocumentsDirectory: URL = {
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
 }()
+
+let dataSaveFailedNotification = Notification.Name("DataSaveFailedNotification")
+
+func fatalCoreDataError(_ error: Error) {
+    print("*** Fatal error: \(error)")
+    NotificationCenter.default.post(name: dataSaveFailedNotification, object: nil)
+}
