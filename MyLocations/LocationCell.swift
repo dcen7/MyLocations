e2932227d4eb8cf5ac4ff1ff784f62e75a2940b3
dcen7 <mdcengiz@gmail.com>
2022-11-17 07:55:37 -0600

    fetch location entities to show on location screen

---
+++ MyLocations/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCell.swift
+//  MyLocations
+//
+//  Created by Mehmet Deniz Cengiz on 11/17/22.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
